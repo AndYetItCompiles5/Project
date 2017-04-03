f97fbeea570d356bec119fae12a41411b7f971e6
AndYetItCompiles5 <zvluetmer@csbsju.edu>
2017-04-03 15:08:22 -0500

    Not sure why it made me commit this

--- tests/AccountControllerTest.java
+++ tests/AccountControllerTest.java
@@ -9,71 +9,81 @@ import Project.*;
 
 public class AccountControllerTest {
 
-	private AccountController aController;
-	private DBController dbController;
-	@Before
-	public void init(){
-		aController = new AccountController();
-		dbController = new DBController();
-		dbController.editAccount("Nathan", "Hansen", "nhansen", "password", 'a', 'Y');
-	}
+ private AccountController aController;
+ private DBController dbController;
+ @Before
+ public void init(){
+  aController = new AccountController();
+  dbController = new DBController();
+  dbController.editAccount("Nathan", "Hansen", "nhansen", "password", 'a', 'Y');
+ }
 
-	@Test
-	public void testLogin() {
-		Assert.assertTrue(aController.login("zakluetmer","password"));
-	}
-	
-	@Test(expected=IllegalArgumentException.class)
-	public void testLoginWrongInfo() {
-		aController.login("wrong","info");
-	}
-	
-	@Test(expected=IllegalArgumentException.class)
-	public void testLoginDeactivated() {
-		aController.login("calseth", "password");
-	}
+ @Test
+ public void testLogin() {
+  Assert.assertTrue(aController.login("zakluetmer","password"));
+ }
+ 
+ @Test(expected=IllegalArgumentException.class)
+ public void testLoginWrongInfo() {
+  aController.login("wrong","info");
+ }
+ 
+ @Test(expected=IllegalArgumentException.class)
+ public void testLoginWrongPassword() {
+  aController.login("zakluetmer","info");
+ }
+ 
+ @Test(expected=IllegalArgumentException.class)
+ public void testLoginWrongUsername() {
+  aController.login("wrong","password");
+ }
+ 
+ @Test(expected=IllegalArgumentException.class)
+ public void testLoginDeactivated() {
+  aController.login("calseth", "password");
+ }
 
-	@Test
-	public void testLogout() {
-		aController.login("zakluetmer","password");
-		Assert.assertTrue(aController.logout().equals(""));
-	}
-	
-	@Test(expected=NullPointerException.class)
-	public void testLogoutNull() {
-		aController.logout();
-	}
+ @Test
+ public void testLogout() {
+  aController.login("zakluetmer","password");
+  Assert.assertTrue(aController.logout().equals(""));
+ }
+ 
+ @Test(expected=NullPointerException.class)
+ public void testLogoutNull() {
+  aController.logout();
+ }
 
-	@Test
-	public void testSetLoggedIn() {
-		aController.setLoggedIn("zakluetmer");
-		Assert.assertTrue(aController.getLoggedIn().equals("zakluetmer"));
-	}
+ @Test
+ public void testSetLoggedIn() {
+  aController.setLoggedIn("zakluetmer");
+  Assert.assertTrue(aController.getLoggedIn().equals("zakluetmer"));
+ }
 
-	//@Test
-	//public void testEditUser() {
-	//	fail("Not yet implemented");
-	//}
+ //@Test
+ //public void testEditUser() {
+ // fail("Not yet implemented");
+ //}
 
-	//@Test
-	//public void testEditAccount() {
-	//	fail("Not yet implemented");
-	//}
+ //@Test
+ //public void testEditAccount() {
+ // fail("Not yet implemented");
+ //}
 
-	@Test
-	public void testDeactivateUser() {
-		Assert.assertTrue(aController.deactivateUser("nhansen"));
-	}
-	
-	@Test(expected=IllegalArgumentException.class)
-	public void testDeactivateUserAlreadyDeactivated() {
-		aController.deactivateUser("calseth");
-	}
+ @Test
+ public void testDeactivateUser() {
+  Assert.assertTrue(aController.deactivateUser("nhansen"));
+ }
+ 
+ @Test(expected=IllegalArgumentException.class)
+ public void testDeactivateUserAlreadyDeactivated() {
+  aController.deactivateUser("calseth");
+ }
 
-	@Test
-	public void testGetLoggedIn() {
-		aController.login("zakluetmer","password");
-		Assert.assertTrue(aController.getLoggedIn().equals("zakluetmer"));
-	}
+ @Test
+ public void testGetLoggedIn() {
+  aController.login("zakluetmer","password");
+  Assert.assertTrue(aController.getLoggedIn().equals("zakluetmer"));
+ }
 
 }
